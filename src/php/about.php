020e6828218d2982936368d16a4cd43694dae4f6
Noah Tarr <noahtarr1@gmail.com>
2019-03-28 20:24:10 -0700

    First Commit

---
+++ src/php/about.php
@@ -0,0 +1,27 @@
+<!--
+ User: noaht
+ Date: 3/24/2019
+-->
+
+<div class="container-fluid" id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12">
+                <div class="row">
+                    <div class="col sectionHeader" id="aboutText">
+                        <h1>About</h1>
+                        <h3>A little about myself.</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p>I am a full stack web developer and programmer based out of Santa Rosa, California. I am currently in school persuing a degree in computer engineering. Beyond programming and web devlopement, I am passionate about road cycling, snowboarding, and live music.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-sm-12" id="aboutImage">
+
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/php/nav.php
@@ -0,0 +1,49 @@
+<!--
+ User: noaht
+ Date: 3/24/2019
+-->
+
+<!--<nav class="navbar navbar-light fixed-top unselectable">-->
+<nav class="navbar navbar-expand-sm navbar-light fixed-top unselectable">
+<!--    <a class="navbar-brand" href="#">Noah Tarr's Portfolio</a>-->
+
+    <!--    For Mobile Support-->
+    <button class="navbar-toggler bg-secondary ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" >
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/resume.pdf" download>Resume</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#timeline">Timeline</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#landingContainer">Top</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+<!--<div class="pos-f-t">-->
+<!--    <nav class="navbar navbar-dark bg-dark">-->
+<!--        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--            <span class="navbar-toggler-icon"></span>-->
+<!--        </button>-->
+<!--    </nav>-->
+<!--    <div class="collapse" id="navbarToggleExternalContent">-->
+<!--        <div class="bg-dark p-4">-->
+<!--            <h4 class="text-white">Collapsed content</h4>-->
+<!--            <span class="text-muted">Toggleable via the navbar brand.</span>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
